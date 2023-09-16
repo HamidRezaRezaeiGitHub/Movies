b3a10f497de53417f604ad67086875e8c3e4004f
Hamidreza Rezaei <hamidreza74hrr@yahoo.com>
2023-09-16 12:23:05 -0400

    API to search by IMDB Id added

--- src/main/java/com/hrr/movies/controller/MovieController.java
+++ src/main/java/com/hrr/movies/controller/MovieController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@RestController 
 @RequestMapping("/api/v1/movies")
 public class MovieController {
 
@@ -27,9 +27,17 @@ public class MovieController {
         return new ResponseEntity<>(movieService.allMovies(), HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Movie> getMovieById(@PathVariable ObjectId id) {
-        Optional<Movie> movieOptional = movieService.movieById(id);
+    @GetMapping("/internalId/{id}")
+    public ResponseEntity<Movie> getMovieByInternalId(@PathVariable ObjectId id) {
+        Optional<Movie> movieOptional = movieService.movieByInternalId(id);
+        return movieOptional
+                .map(movie -> new ResponseEntity<>(movie, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable String imdbId) {
+        Optional<Movie> movieOptional = movieService.movieById(imdbId);
         return movieOptional
                 .map(movie -> new ResponseEntity<>(movie, HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
