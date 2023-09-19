a019e51197d6e3acdfb9f89541fbd2a40a461de4
Hamidreza Rezaei <hamidreza74hrr@yahoo.com>
2023-09-18 23:55:50 -0400

    CrossOrigin annotation added to controllers

--- src/main/java/com/hrr/movies/controller/MovieController.java
+++ src/main/java/com/hrr/movies/controller/MovieController.java
@@ -5,15 +5,13 @@ import com.hrr.movies.service.MovieService;
 import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000/", maxAge = 3600)
 @RequestMapping("/api/v1/movies")
 public class MovieController {
 
--- src/main/java/com/hrr/movies/controller/ReviewController.java
+++ src/main/java/com/hrr/movies/controller/ReviewController.java
@@ -11,6 +11,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000/", maxAge = 3600)
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
 
