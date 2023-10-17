17a6b769615f0d725edeb1e0e86b1dab32b9db14
Hamidreza Rezaei <hamidreza74hrr@yahoo.com>
2023-10-16 20:50:47 -0400

    Page not found component added

--- front-client/src/App.js
+++ front-client/src/App.js
@@ -7,6 +7,7 @@ import Home from "./components/home/Home";
 import Header from "./components/header/Header";
 import Trailer from "./components/trailer/Trailer";
 import Reviews from "./components/reviews/Reviews";
+import NotFound from "./components/notfound/NotFound";
 
 function App() {
   const [movies, setMovies] = useState();
@@ -63,6 +64,7 @@ function App() {
                 setReviews={setReviews}
               />
             }></Route>
+          <Route path="*" element={<NotFound />}></Route>
         </Route>
       </Routes>
     </div>
